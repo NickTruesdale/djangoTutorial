127bd00b97bd4ba284435270196a8c891497a30f
Nick <truesdalen@gmail.com>
2017-01-31 17:58:53 +0000

    Added HTML to main view

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ttjr3!!28htia=wuu&5oj4kiyh3ni_=tsd12_^rvbb7kcpu*o*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'Sticky073.pythonanywhere.com']
 
 
 # Application definition
